93197eeb740293abc6f6be4782e32d19edf54f00
wahujos <jwahu30@gmail.com>
2024-05-07 22:25:25 +0300

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,11 @@ documenting the module
 """
 import asyncio
 import random
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Documenting the function
     """
