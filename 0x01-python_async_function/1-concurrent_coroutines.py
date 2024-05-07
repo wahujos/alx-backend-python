a9a5f5da27cb30b4ea517b8919afdeabd4aafd69
wahujos <jwahu30@gmail.com>
2024-05-07 12:26:15 -0700

    1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+documenting the module
+"""
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """Documenting the function"""
+    delays = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*tasks)
+
+    delays.extend(result)
+    delays.sort()
+    return delays
